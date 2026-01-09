fe4f3d6937c56d95c484faeb58c5bad2d6cd9a60
shivangiupadhyayyy <ushivangi478@gmail.com>
2026-01-09 23:41:48 +0530

    Update analytics.py

--- src/analytics.py
+++ src/analytics.py
@@ -10,15 +10,14 @@ class AnalyticsEngine:
         if brand_data.empty:
             return None
 
-        # 1. Best performing format based on Weighted Score
+        
         avg_engagement = brand_data.groupby('post_type')['engagement_score'].mean()
         best_format = avg_engagement.idxmax()
 
-        # 2. Get Top 3 Best Posts (Rich Data)
-        # Ab hum URL aur Location bhi return karenge
+        
         top_posts = brand_data.sort_values(by='engagement_score', ascending=False).head(3)
         
-        # Convert top posts to a list of dictionaries
+        
         top_themes = []
         for _, row in top_posts.iterrows():
             top_themes.append({
@@ -29,6 +28,7 @@ class AnalyticsEngine:
 
         return {
             'best_format': best_format,
-            'top_themes': top_themes, # Now contains objects, not just strings
+            'top_themes': top_themes, 
             'avg_engagement': brand_data['engagement_score'].mean()
+
         }
